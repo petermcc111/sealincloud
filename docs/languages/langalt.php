467793cb6737d4c376dc1517df457d9fc5643b6c
Peter <peter.mcchau@gmail.com>
2024-09-22 20:20:17 +0800

    Add files via upload

---
+++ docs/languages/langalt.php
@@ -0,0 +1,56 @@
+<?
+//echo($_COOKIE["lang"]);
+
+if (!empty($_GET["lang"])){
+	setcookie("lang", $_GET["lang"]);
+	$_COOKIE["lang"] = $_GET["lang"];
+}
+var_dump(empty($_COOKIE["lang"]));
+if (empty($_COOKIE["lang"])){
+	echo("no lang");
+	//setcookie("lang", "en_US");
+}
+//echo("get cookie...");
+echo($_COOKIE["lang"]);
+//echo("end cookie...");
+//echo("<br/>");
+//$myfile = fopen("book.json", "r") or die("Unable to open file!");
+//$mycontent = fread($myfile,filesize("book.json"));
+if (empty($book)){
+	$book = "book.json";
+}
+$myfile = fopen($book, "r") or die("Unable to open file!");
+$mycontent = fread($myfile,filesize($book));
+
+$mydict = json_decode($mycontent,true);
+//var_dump($mydict);
+/*
+foreach($mydict as $i){
+	if ($i["key"]=="Gettext Multi-language website demo"){
+		var_dump($i["zh_cn"]);
+		echo("<br/>");
+		var_dump($i["zh_hk"]);
+	}
+}
+*/
+
+
+
+function _($key){
+	//echo("<br/>");
+	//var_dump(gettype($GLOBALS["mydict"]));
+	
+	foreach ($GLOBALS["mydict"] as $x) {
+		if ($x["key"] == $key){
+			//var_dump($i[$_COOKIE["lang"]]);
+			if (empty($_COOKIE["lang"])){
+				return $x["en_US"];
+			}else{
+
+				return $x[$_COOKIE["lang"]];
+			}
+		}
+	}
+}
+
+//var_dump(_('Gettext Multi-language website demo'));
